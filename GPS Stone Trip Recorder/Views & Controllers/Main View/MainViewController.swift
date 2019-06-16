6d5ad48614ec472d78a68bc0df273a2da82c6dad
Frizlab <fload@me.com>
2019-06-16 20:46:35 +0200

    Move stuff around in the Main storyboard and create the MainViewController swift class

---
+++ GPS Stone Trip Recorder/Views & Controllers/Main View/MainViewController.swift
@@ -0,0 +1,18 @@
+/*
+ * MainViewController.swift
+ * GPS Stone Trip Recorder
+ *
+ * Created by François Lamboley on 2019/6/16.
+ * Copyright © 2019 Frost Land. All rights reserved.
+ */
+
+import Foundation
+import UIKit
+
+
+
+class MainViewController : UIViewController {
+	
+	
+	
+}
