078bc656935101f372f57c55595f6e3677275e5d
Frizlab <fload@me.com>
2019-06-19 18:47:41 +0200

    Add the SettingsViewController class

---
+++ GPS Stone Trip Recorder/Views & Controllers/Settings View/SettingsViewController.swift
@@ -0,0 +1,20 @@
+/*
+ * SettingsViewController.swift
+ * GPS Stone Trip Recorder
+ *
+ * Created by François Lamboley on 19/06/2019.
+ * Copyright © 2019 Frost Land. All rights reserved.
+ */
+
+import Foundation
+import UIKit
+
+
+
+class SettingsViewController : UITableViewController {
+	
+	@IBOutlet var segmentedCtrlMapType: UISegmentedControl!
+	@IBOutlet var textFieldMinDist: UITextField!
+	@IBOutlet var textFieldMinTime: UITextField!
+	
+}
