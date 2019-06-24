3513d36bb7a6153f1b839c4e1ac506ab48eb39d0
Frizlab <fload@me.com>
2019-06-24 18:45:50 +0200

    Create UIUtils (nothing in it yet)

---
+++ GPS Stone Trip Recorder/Views & Controllers/Global/UIUtils.swift
@@ -0,0 +1,23 @@
+/*
+ * UIUtils.swift
+ * GPS Stone Trip Recorder
+ *
+ * Created by François Lamboley on 24/06/2019.
+ * Copyright © 2019 Frost Land. All rights reserved.
+ */
+
+import Foundation
+import UIKit
+
+
+
+struct UIUtils {
+	
+	static func endRecording(from viewController: UIViewController) {
+		
+//		viewController.present(<#T##viewControllerToPresent: UIViewController##UIViewController#>, animated: <#T##Bool#>, completion: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
+	}
+	
+	private init() {}
+	
+}
