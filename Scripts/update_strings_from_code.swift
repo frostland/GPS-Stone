21e81d8a4cc5e06c19c65c2c77fa6223c3d0dd2d
Frizlab <fload@me.com>
2020-06-06 18:47:26 +0200

    Add script to update strings file from code/storyboards

---
+++ Scripts/update_strings_from_code.swift
@@ -0,0 +1,33 @@
+#!/usr/bin/swift sh
+
+import Foundation
+
+import SwiftShell // @kareman ~> 5.0.1
+
+
+/* swift-sh creates a binary whose path is not one we expect, so we cannot use
+ * main.path directly.
+ * Using the _ env variable is **extremely** hacky, but seems to do the job…
+ * See https://github.com/mxcl/swift-sh/issues/101 */
+let filepath = ProcessInfo.processInfo.environment["_"] ?? main.path
+main.currentdirectory = URL(fileURLWithPath: filepath).deletingLastPathComponent().appendingPathComponent("..").path
+
+
+
+do {
+	guard main.arguments.count == 0 else {
+		exit(errormessage: "usage: \(filepath)")
+	}
+	
+	try runAndPrint(
+		"locmapper", "update_xcode_strings_from_code",
+		"--encoding=utf8", "--delete-missing-keys",
+		"--unlocalized-xibs-files-list=.locmapper__unlocalized_xibs",
+		"--unused-stringsfiles-files-list=.locmapper__unused_stringfiles",
+		"--localizables-path=GPS Stone/Supporting Files/Localizables",
+		"--exclude-list=.git/,gfx building/,App Store/,Scripts/",
+		"."
+	)
+} catch {
+	exit(error)
+}
