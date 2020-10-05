cd7b10d28bd27b8a57806964e1f8449eb8489c9f
Frizlab <fload@me.com>
2020-10-05 23:33:00 +0200

    Create a sync version of the GPX export preparation

--- GPS Stone/Heart/Services/RecordingExporter.swift
+++ GPS Stone/Heart/Services/RecordingExporter.swift
@@ -17,6 +17,10 @@ import XibLoc
 
 final class RecordingExporter {
 	
+	static func recordingName(of recordingID: NSManagedObjectID, context: NSManagedObjectContext) -> String {
+		return (try? context.performAndWait{ try Recording.existingFrom(id: recordingID, in: context).name }) ?? RecordingExporter.defaultRecordingName
+	}
+	
 	init(dataHandler: DataHandler) {
 		dh = dataHandler
 	}
@@ -38,97 +42,65 @@ final class RecordingExporter {
 		let progress = Progress()
 		let context = dh.bgContext
 		context.perform{
-			do {
-				let fileManager = FileManager.default
-				let recording = try Recording.existingFrom(id: recordingID, in: context)
-				
-				let outputURL = try self.urlForRecordingID(recordingID, recordingName: recording.name)
-				let inprogressOutputURL = outputURL.appendingPathExtension("inprogress")
-				
-				/* First let’s check if the file has not already been created. Note:
-				 * We also check if the file existing, if it exists, is indeed a
-				 * file and not a directory. If it is a directory, we’ll delete it. */
-				var isDir = ObjCBool(true)
-				let outputExists = fileManager.fileExists(atPath: outputURL.path, isDirectory: &isDir)
-				guard !outputExists || isDir.boolValue else {
-					DispatchQueue.main.async{
-						handler(.success(outputURL))
-					}
-					return
-				}
-				
-				if outputExists {try fileManager.removeItem(at: outputURL)}
-				
-				/* We now have a clean slate. Let’s work. */
-				try Data().write(to: inprogressOutputURL)
-				let fileHandle = try FileHandle(forWritingTo: inprogressOutputURL)
-				defer {
-					if #available(iOS 13.0, *) {_ = try? fileHandle.close()}
-					else                       {fileHandle.closeFile()}
-				}
-				
-				/* Write the preamble. */
-				let creator = NSLocalizedString("gpx creator tag", comment: "The text in the “creator” field of the GPX exports.")
-					.applyingCommonTokens(simpleReplacement1: (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String).flatMap{ " " + $0 } ?? "")
-				let preamble = """
-					<?xml version="1.0" encoding="UTF-8"?>
-					<gpx creator="\(self.xmlString(creator))" version="1.1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.topografix.com/GPX/1/1" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">
-						<trk>
-					
-					"""
-				fileHandle.write(Data(preamble.utf8))
-				
-				/* Write the points. */
-				var curSegment: Int16?
-				let points = try recording.pointsSortedByDateAscending()
-				progress.totalUnitCount = Int64(points.count)
-				for point in points {
-					defer {progress.completedUnitCount += 1}
-					
-					guard let pointLocation = point.location, pointLocation.horizontalAccuracy.sign == .plus else {continue}
-					
-					let rawMagvar = point.heading?.trueHeading ?? pointLocation.course
-					let magvar = (rawMagvar.sign == .plus ? rawMagvar : nil)
-					let altitude = (pointLocation.verticalAccuracy.sign == .plus ? pointLocation.altitude : nil)
-					
-					/* Write segment start, with previous segment end if needed. */
-					if curSegment != point.segmentID {
-						if curSegment != nil {
-							let segmentEnd = """
-										</trkseg>
-								
-								"""
-							fileHandle.write(Data(segmentEnd.utf8))
-						}
-						let segmentStart = """
-									<trkseg>
-							
-							"""
-						fileHandle.write(Data(segmentStart.utf8))
-					}
-					curSegment = point.segmentID
-					
-					/* Write the segment point. */
-					let latStr = String(format: "%.10f", pointLocation.coordinate.latitude)
-					let lonStr = String(format: "%.10f", pointLocation.coordinate.latitude)
-					let hdopStr = String(format: "%f", pointLocation.horizontalAccuracy)
-					let magvarStr = magvar.flatMap{ String(format: "%f", $0) }
-					let altitudeStr = altitude.flatMap{ String(format: "%f", $0) }
-					let verticalAccuracyStr = altitude.flatMap{ _ in String(format: "%f", pointLocation.verticalAccuracy) }
-					let pointLines = [
-						                             #"\#t\#t\#t<trkpt lat="\#(self.xmlString(latStr))" lon="\#(self.xmlString(lonStr))">"#,
-						point.date.flatMap{          #"\#t\#t\#t\#t<time>\#(self.xmlString(self.isoStringFromDate($0)))</time>"# },
-						                             #"\#t\#t\#t\#t<hdop>\#(self.xmlString(hdopStr))</hdop>"#,
-						magvarStr.flatMap{           #"\#t\#t\#t\#t<magvar>\#(self.xmlString($0))</magvar>"# },
-						altitudeStr.flatMap{         #"\#t\#t\#t\#t<ele>\#(self.xmlString($0))</ele>"# },
-						verticalAccuracyStr.flatMap{ #"\#t\#t\#t\#t<vdop>\#(self.xmlString($0))</vdop>"# },
-						                             #"\#t\#t\#t</trkpt>"#,
-						""
-					].compactMap{ $0 }
-					fileHandle.write(Data(pointLines.joined(separator: "\n").utf8))
-				}
-				
-				/* Write the end. */
+			let r = Result{ try self.prepareExportSyncOnContext(of: recordingID, context: context, progress: progress) }
+			DispatchQueue.main.async{ handler(r) }
+		}
+		return progress
+	}
+	
+	func prepareExportSyncOnContext(of recordingID: NSManagedObjectID, context: NSManagedObjectContext, progress: Progress?) throws -> URL {
+		let fileManager = FileManager.default
+		let recording = try Recording.existingFrom(id: recordingID, in: context)
+		
+		let outputURL = try urlForRecordingID(recordingID, recordingName: recording.name)
+		let inprogressOutputURL = outputURL.appendingPathExtension("inprogress")
+		
+		/* First let’s check if the file has not already been created. Note: We
+		 * also check if the file existing, if it exists, is indeed a file and not
+		 * a directory. If it is a directory, we’ll delete it. */
+		var isDir = ObjCBool(true)
+		let outputExists = fileManager.fileExists(atPath: outputURL.path, isDirectory: &isDir)
+		guard !outputExists || isDir.boolValue else {
+			return outputURL
+		}
+		
+		if outputExists {try fileManager.removeItem(at: outputURL)}
+		
+		/* We now have a clean slate. Let’s work. */
+		try Data().write(to: inprogressOutputURL)
+		let fileHandle = try FileHandle(forWritingTo: inprogressOutputURL)
+		defer {
+			if #available(iOS 13.0, *) {_ = try? fileHandle.close()}
+			else                       {fileHandle.closeFile()}
+		}
+		
+		/* Write the preamble. */
+		let creator = NSLocalizedString("gpx creator tag", comment: "The text in the “creator” field of the GPX exports.")
+			.applyingCommonTokens(simpleReplacement1: (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String).flatMap{ " " + $0 } ?? "")
+		let preamble = """
+			<?xml version="1.0" encoding="UTF-8"?>
+			<gpx creator="\(xmlString(creator))" version="1.1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.topografix.com/GPX/1/1" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">
+				<trk>
+			
+			"""
+		fileHandle.write(Data(preamble.utf8))
+		
+		/* Write the points. */
+		var curSegment: Int16?
+		let points = try recording.pointsSortedByDateAscending()
+		progress?.totalUnitCount = Int64(points.count)
+		for point in points {
+			Thread.sleep(forTimeInterval: 1)
+			defer {progress?.completedUnitCount += 1}
+			
+			guard let pointLocation = point.location, pointLocation.horizontalAccuracy.sign == .plus else {continue}
+			
+			let rawMagvar = point.heading?.trueHeading ?? pointLocation.course
+			let magvar = (rawMagvar.sign == .plus ? rawMagvar : nil)
+			let altitude = (pointLocation.verticalAccuracy.sign == .plus ? pointLocation.altitude : nil)
+			
+			/* Write segment start, with previous segment end if needed. */
+			if curSegment != point.segmentID {
 				if curSegment != nil {
 					let segmentEnd = """
 								</trkseg>
@@ -136,28 +108,55 @@ final class RecordingExporter {
 						"""
 					fileHandle.write(Data(segmentEnd.utf8))
 				}
-				let end = """
-						</trk>
-					</gpx>
+				let segmentStart = """
+							<trkseg>
 					
 					"""
-				fileHandle.write(Data(end.utf8))
-				
-				try fileManager.moveItem(at: inprogressOutputURL, to: outputURL)
-				
-				DispatchQueue.main.async{
-					handler(.success(outputURL))
-				}
-			} catch {
-				DispatchQueue.main.async{
-					handler(.failure(error))
-				}
+				fileHandle.write(Data(segmentStart.utf8))
 			}
+			curSegment = point.segmentID
+			
+			/* Write the segment point. */
+			let latStr = String(format: "%.10f", pointLocation.coordinate.latitude)
+			let lonStr = String(format: "%.10f", pointLocation.coordinate.latitude)
+			let hdopStr = String(format: "%f", pointLocation.horizontalAccuracy)
+			let magvarStr = magvar.flatMap{ String(format: "%f", $0) }
+			let altitudeStr = altitude.flatMap{ String(format: "%f", $0) }
+			let verticalAccuracyStr = altitude.flatMap{ _ in String(format: "%f", pointLocation.verticalAccuracy) }
+			let pointLines = [
+													  #"\#t\#t\#t<trkpt lat="\#(xmlString(latStr))" lon="\#(xmlString(lonStr))">"#,
+				point.date.flatMap{          #"\#t\#t\#t\#t<time>\#(xmlString(isoStringFromDate($0)))</time>"# },
+													  #"\#t\#t\#t\#t<hdop>\#(xmlString(hdopStr))</hdop>"#,
+				magvarStr.flatMap{           #"\#t\#t\#t\#t<magvar>\#(xmlString($0))</magvar>"# },
+				altitudeStr.flatMap{         #"\#t\#t\#t\#t<ele>\#(xmlString($0))</ele>"# },
+				verticalAccuracyStr.flatMap{ #"\#t\#t\#t\#t<vdop>\#(xmlString($0))</vdop>"# },
+													  #"\#t\#t\#t</trkpt>"#,
+				""
+				].compactMap{ $0 }
+			fileHandle.write(Data(pointLines.joined(separator: "\n").utf8))
 		}
-		return progress
+		
+		/* Write the end. */
+		if curSegment != nil {
+			let segmentEnd = """
+						</trkseg>
+				
+				"""
+			fileHandle.write(Data(segmentEnd.utf8))
+		}
+		let end = """
+				</trk>
+			</gpx>
+			
+			"""
+		fileHandle.write(Data(end.utf8))
+		
+		try fileManager.moveItem(at: inprogressOutputURL, to: outputURL)
+		
+		return outputURL
 	}
 	
-	private static let defaultRecordingName = NSLocalizedString("default recording name", comment: "The name of a recording when no name is given.")
+	static let defaultRecordingName = NSLocalizedString("default recording name", comment: "The name of a recording when no name is given.")
 	
 	private let dh: DataHandler
 	
