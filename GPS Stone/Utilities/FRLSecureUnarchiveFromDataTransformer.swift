5db738c8bb441c7cb97be8243616b8945de53cc1
Frizlab <fload@me.com>
2020-11-01 09:41:13 +0100

    Use a custom secure transformer to allow deploying to iOS 11

--- GPS Stone/Utilities/FRLSecureUnarchiveFromDataTransformer.swift
+++ GPS Stone/Utilities/FRLSecureUnarchiveFromDataTransformer.swift
@@ -11,21 +11,37 @@ import Foundation
 
 
 
+/* We could override NSSecureUnarchiveFromDataTransformer (see commit
+ * 1f05fa28fb31f2f592b53d1c9dac08ff8bd39066), but it’s only available from
+ * iOS 12. */
+
 /**
-An NSSecureUnarchiveFromDataTransformer subclass that allows more top level
-classes. */
+An ValueTransformer subclass that allows secure decoding of the classes we need.
+
+- Note: This implementation does not care if the object you give it are not of
+the expected class, but decoding through this transformer _will_ fail if you try
+and decode an archive containing an object of the incorrect class. */
 @objc(FRLSecureUnarchiveFromDataTransformer)
-final class FRLSecureUnarchiveFromDataTransformer : NSSecureUnarchiveFromDataTransformer {
+final class FRLSecureUnarchiveFromDataTransformer : ValueTransformer {
 	
 	static let name = NSValueTransformerName(rawValue: String(describing: FRLSecureUnarchiveFromDataTransformer.self))
-	
-	override static var allowedTopLevelClasses: [AnyClass] {
-		return super.allowedTopLevelClasses + [CLLocation.self, CLHeading.self]
-	}
 	
 	public static func register() {
 		let transformer = FRLSecureUnarchiveFromDataTransformer()
 		ValueTransformer.setValueTransformer(transformer, forName: name)
+	}
+	
+	override class func transformedValueClass() -> AnyClass {
+		return NSData.self
+	}
+	
+	override func transformedValue(_ value: Any?) -> Any? {
+		return try? NSKeyedArchiver.archivedData(withRootObject: value as Any, requiringSecureCoding: true)
+	}
+	
+	override func reverseTransformedValue(_ value: Any?) -> Any? {
+		guard let value = value as? Data else {return nil}
+		return try? NSKeyedUnarchiver.unarchivedObject(ofClasses: [CLLocation.self, CLHeading.self], from: value)
 	}
 	
 }
