c2b690cb48210fbd985bded6a3811b913869cd49
François Lamboley <francois.lamboley@frostland.fr>
2020-12-24 09:57:43 +0100

    Implement asking before pausing or stopping a recording

--- GPS Stone/Utilities/Utils+UI.swift
+++ GPS Stone/Utilities/Utils+UI.swift
@@ -31,4 +31,66 @@ extension Utils {
 		}
 	}
 	
+	static func startOrResumeRecording(in viewController: UIViewController, using locationRecorder: LocationRecorder) {
+		Utils.executeOrShowAlertIn(viewController){
+			switch locationRecorder.recStatus {
+				case .stopped: try locationRecorder.startNewRecording()
+				case .paused:  try locationRecorder.resumeCurrentRecording()
+				case .recording: (/*nop*/)
+			}
+		}
+	}
+	
+	static func pauseRecording(in viewController: UIViewController, using locationRecorder: LocationRecorder, appSettings: AppSettings) {
+		let action = {
+			Utils.executeOrShowAlertIn(viewController){
+				switch locationRecorder.recStatus {
+					case .recording: _ = try locationRecorder.pauseCurrentRecording()
+					case .stopped, .paused: (/*nop*/)
+				}
+			}
+		}
+		
+		if !appSettings.askBeforePausingOrStopping {
+			action()
+		} else {
+			let alertVC = UIAlertController(
+				title:   NSLocalizedString("avt: pause the recording", comment:   "Title of the alert view before pausing a recording."),
+				message: NSLocalizedString("avm: pause the recording", comment: "Message of the alert view before pausing a recording."),
+				preferredStyle: .alert
+			)
+			let cancelButtonText = NSLocalizedString("Cancel", comment: "")
+			let confirmButtonText = NSLocalizedString("avb: confirm pause the recording", comment: "Text of the button to confirm pausing the recording in the alert view before pausing a recording.")
+			alertVC.addAction(UIAlertAction(title: cancelButtonText,  style: .cancel,  handler: { _ in }))
+			alertVC.addAction(UIAlertAction(title: confirmButtonText, style: .default, handler: { _ in action() }))
+			viewController.present(alertVC, animated: true, completion: nil)
+		}
+	}
+	
+	static func stopRecording(in viewController: UIViewController, using locationRecorder: LocationRecorder, appSettings: AppSettings) {
+		let action = {
+			Utils.executeOrShowAlertIn(viewController){
+				switch locationRecorder.recStatus {
+					case .recording, .paused: _ = try locationRecorder.stopCurrentRecording()
+					case .stopped: (/*nop*/)
+				}
+			}
+		}
+		
+		if !appSettings.askBeforePausingOrStopping {
+			action()
+		} else {
+			let alertVC = UIAlertController(
+				title:   NSLocalizedString("avt: stop the recording", comment:   "Title of the alert view before stopping a recording."),
+				message: NSLocalizedString("avm: stop the recording", comment: "Message of the alert view before stopping a recording."),
+				preferredStyle: .alert
+			)
+			let cancelButtonText = NSLocalizedString("Cancel", comment: "")
+			let confirmButtonText = NSLocalizedString("avb: confirm stop the recording", comment: "Text of the button to confirm stopping the recording in the alert view before stopping a recording.")
+			alertVC.addAction(UIAlertAction(title: cancelButtonText,  style: .cancel,  handler: { _ in }))
+			alertVC.addAction(UIAlertAction(title: confirmButtonText, style: .default, handler: { _ in action() }))
+			viewController.present(alertVC, animated: true, completion: nil)
+		}
+	}
+	
 }
